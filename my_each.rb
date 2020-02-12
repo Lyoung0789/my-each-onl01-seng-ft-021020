4c35b84c0ffaf8987db795608d102b3752ecf8e8
Lester Young <lyoung070789@gmail.com>
2020-02-12 04:51:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(array)
-  if block_given?
+ # if block_given?
     i = 0 
     while i<array.length
       yield array[i]
         i+=1
     end
-  else
-    array
-  end
+  #else
+   # array
+  #end
   array
 end
